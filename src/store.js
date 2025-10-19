bb6b2d274c27d37b0e2f667c7d744e89b973293b
Eman Salama <eg1712@gmail.com>
2025-10-19 04:28:21 +0300

    Update Cart Item Final

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
+import { addItem, updateQuantity, removeItem } from './CartSlice';
+
  const store = configureStore({
     reducer: {
         cart: cartReducer,
